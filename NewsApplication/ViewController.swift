dec9fef9a4d003003dc7698411a50df5a8abda86
Akbar Umetov <aumetov7@gmail.com>
2020-08-05 19:37:07 +0300

    Initial Commit

---
+++ NewsApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApplication
+//
+//  Created by Акбар Уметов on 05.08.2020.
+//  Copyright © 2020 Akbar Umetov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
